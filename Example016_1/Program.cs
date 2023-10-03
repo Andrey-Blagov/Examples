6436a92ac2f59bfaa73a9d61db1558ccb647e18c
Andrey <blagovandrey1323@gmail.com>
2023-10-03 14:15:06 +0300

    add E16_1

---
+++ Example016_1/Program.cs
@@ -0,0 +1,34 @@
+﻿/* int Factorial (int n);
+{
+    if (n == 1) return 1;
+    else return n * Factorial(n-1);
+}
+for (int i = 1; i < 40; i++)
+{
+  Console.WriteLine($"{i}! = {Factorial(i)}");
+}
+ */
+
+using System;
+
+namespace Factorial
+{
+   class Program
+   {
+        static void Main(string[] args)
+        {
+             double Factorial(int n)
+             {
+                 if (n == 1)
+                 return 1;
+                 else
+                 return n * Factorial(n - 1);
+             }
+
+             for (int i = 1; i < 40; i++)
+             {
+                 Console.WriteLine($"{i}! = {Factorial(i)}");
+             }
+        }
+    }
+}
